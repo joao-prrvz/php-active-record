e51610176d49679a23beefca424559fa5fe9fa0d
Joao prrvz <joao.prrvz@eduge.ch>
2026-09-09 10:38:30 +0200

    Create Table Attribute to define table name in the database

---
+++ src/Attributes/Table.php
@@ -0,0 +1,11 @@
+<?php
+namespace PHPActiveRecord\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Table
+{
+    public function __construct(public string $name)
+    { }
+}
