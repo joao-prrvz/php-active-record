e45b6a64d02ebc15d39e9de0b8a21ab8a0974342
Joao prrvz <joao.prrvz@eduge.ch>
2026-09-09 14:39:10 +0200

    Update ActiveRecord: - Add foreign key support - Add include method for foreign key objects - Add getPrimaryKey method - Add exec method - Made instanciate method public

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -1,18 +1,20 @@
 <?php
 namespace PHPActiveRecord;
 
+use Exception;
 use Override;
 use PDO;
 use PDOStatement;
+use PHPActiveRecord\Attributes\ForeignKey;
 use PHPActiveRecord\Attributes\Table;
 use ReflectionAttribute;
 use ReflectionClass;
+use ReflectionNamedType;
 use ReflectionProperty;
 
 abstract class ActiveRecord implements IActiveRecord
 {
     private static PDO $_pdo;
-
     protected static IQueryBuilder $builder;
     
     /**
@@ -49,12 +51,24 @@ abstract class ActiveRecord implements IActiveRecord
     public static function getColumns(): array
     {
         $refProps = new ReflectionClass(static::class)->getProperties();
-        $refProps = array_filter($refProps, fn(ReflectionProperty $p) => $p->getDeclaringClass()->name == static::class);
+        $refProps = array_filter($refProps, function(ReflectionProperty $p) {
+            return $p->getDeclaringClass()->name == static::class &&
+            count($p->getAttributes(ForeignKey::class)) < 1;
+        });
         $columns = [];
-        foreach ($refProps as $refProp) {
+        foreach ($refProps as $refProp)
             $columns[$refProp->name] = $refProp->name;
-        }
         return $columns;
+    }
+
+    /**
+     * Property used as primary key in the model
+     *
+     * @return ReflectionProperty
+     */
+    public static function getPrimaryKey(): ReflectionProperty
+    {
+        return new ReflectionClass(static::class)->getProperty("id");
     }
 
     /**
@@ -72,7 +86,7 @@ abstract class ActiveRecord implements IActiveRecord
     }
 
     /**
-     * Prepares and executes an SQL query
+     * Prepares and executes an SQL statement
      *
      * @param string $sql
      * @param array<string, mixed> $params
@@ -80,24 +94,37 @@ abstract class ActiveRecord implements IActiveRecord
      */
     public static function run(string $sql, array $params = []): PDOStatement
     {
-        $sttmt = self::$_pdo->prepare($sql);
+        $sttmt = self::getPDO()->prepare($sql);
         $sttmt->execute($params);
         return $sttmt;
     }
 
+    /**
+     * Directly runs an SQL query
+     *
+     * @param string $sql
+     * @return void
+     */
+    public static function exec(string $sql): void
+    {
+        self::getPDO()->exec($sql);
+    }
+
+
+
     #[Override]
     public function save(): void
     {
-        $propName = "id";
         $ref = new ReflectionClass($this);
-        $refProp = $ref->getProperty($propName);
-        if (!$refProp->isInitialized($this))
-            $this->$propName = $this->insert();
+        $refPK = static::getPrimaryKey();
+        if (!$refPK->isInitialized($this))
+            $refPK->setValue($this, $this->insert());
         else
             $this->update();
-        $obj = static::findById($this->$propName);
-        foreach (static::getColumns() as $prop => $column) {
-            $this->$prop = $obj->$prop;
+        $obj = static::findById($refPK->getValue($this));
+        foreach (static::getColumns() as $propName => $column) {
+            $refProp =  $ref->getProperty($propName);
+            $refProp->setValue($this, $refProp->getValue($obj));
         }
     }
 
@@ -109,11 +136,9 @@ abstract class ActiveRecord implements IActiveRecord
             new QueryCondition("`$table`.`id` = ?")
         ]);
         $params = [];
-        foreach ($columns as $prop => $column) {
+        foreach ($columns as $prop => $column) 
             $params[] = $this->$prop;
-        }
-        $propName = "id";
-        $params[] = $this->$propName;
+        $params[] = static::getPrimaryKey()->getValue($this);
         $this->run($sql, $params);
     }
 
@@ -123,9 +148,8 @@ abstract class ActiveRecord implements IActiveRecord
         unset($columns["id"]);
         $sql = static::$builder->insert(static::getTable(), $columns);
         $params = [];
-        foreach ($columns as $prop => $column) {
+        foreach ($columns as $prop => $column)
             $params[] = $this->$prop;
-        }
         static::run($sql, $params);
         return (int)static::getPDO()->lastInsertId();
     }
@@ -137,9 +161,8 @@ abstract class ActiveRecord implements IActiveRecord
         $sttmt = static::run($sql);
         $data = $sttmt->fetchAll();
         $result = [];
-        foreach ($data as $value) {
+        foreach ($data as $value)
             $result[] = static::instanciate($value);
-        }
         return $result;
     }
 
@@ -164,8 +187,64 @@ abstract class ActiveRecord implements IActiveRecord
         $sql = self::$builder->delete($table, [
             new QueryCondition("`$table`.`id` = ?")
         ]);
-        $propName = "id";
-        static::run($sql, [$this->$propName]);
+        static::run($sql, [static::getPrimaryKey()->getValue($this)]);
+    }
+
+    /**
+     * Requests for an object with the attribute {@see ForeignKey}
+     *
+     * @param string $propertyName
+     * @return void
+     */
+    public function include(string $propertyName): void
+    {
+        $ref = new ReflectionClass($this);
+        $refProp = $ref->getProperty($propertyName);
+        $refAttr = $refProp->getAttributes(ForeignKey::class)[0] ?? null;
+        if ($refAttr == null)
+            throw new Exception("The property '$propertyName' doesn't have the attribute ForeignKey");
+        $refType = $refProp->getType();
+        if (!($refType instanceof ReflectionNamedType))
+            throw new Exception("A foreign object must be an array or anotner model's class");
+        $typeName = $refType->getName(); 
+        if ($typeName === "array") {
+            $this->includeMultiple($refProp, $refAttr->newInstance());
+            return;
+        }
+        if (!class_exists($typeName))
+            throw new Exception("A foreign object must be an array or anotner model's class");
+        $this->includeSingle($refProp, $refAttr->newInstance());
+    }
+
+    private function includeSingle(ReflectionProperty $refProp, ForeignKey $attr): void
+    {
+        $refType = $refProp->getType();
+        $table = static::getTable();
+        $sql = static::$builder->select(static::getTable(), [$attr->name], [
+            new QueryCondition("`$table`.`id` = ?")
+        ]);
+        $sttmt = static::run($sql, [static::getPrimaryKey()->getValue($this)]);
+        $fk = (int)$sttmt->fetch()[$attr->name];
+        if ($refType instanceof ReflectionNamedType) {
+            $refMet = new ReflectionClass($refType->getName())->getMethod("findById");
+            $obj = $refMet->invoke(null, $fk);
+            $refProp->setValue($this, $obj);
+        }
+    }
+
+    private function includeMultiple(ReflectionProperty $refProp, ForeignKey $attr): void
+    {
+        $table = $attr->type::getTable();
+        $sql = static::$builder->select($table, $attr->type::getColumns(), [
+            new QueryCondition("`$table`.`$attr->name` = ?")
+        ]);
+
+        $sttmt = static::run($sql, [static::getPrimaryKey()->getValue($this)]);
+        $data = $sttmt->fetchAll();
+        $values = [];
+        foreach ($data as $row)
+            $values[] = $attr->type::instanciate($row);
+        $refProp->setValue($this, $values);
     }
 
     /**
@@ -174,7 +253,7 @@ abstract class ActiveRecord implements IActiveRecord
      * @param array $data
      * @return static
      */
-    protected static function instanciate(array $data): object
+    public static function instanciate(array $data): object
     {
         $refClass = new ReflectionClass(static::class);
         $obj = $refClass->newInstance();
